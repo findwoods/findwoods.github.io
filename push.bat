d725b0a45adaa938e5adb5f514ca945f4f4a6202
findwoods <shanewoods@g.ucla.edu>
2025-05-20 15:32:11 -0400

    Auto-sync after Windows file changes

---
+++ push.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d D:\Columbia\findwoods\findwoods.github.io
+
+echo Adding all changes...
+git add .
+
+echo Committing changes...
+git commit -m "Auto-sync after Windows file changes"
+
+echo Pushing to GitHub...
+git push origin main
+
+pause
